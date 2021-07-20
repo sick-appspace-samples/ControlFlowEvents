dfa0191b9f32e276289aa663ce960ef973270483
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 13:15:29 +0200

    'Update'

--- ControlFlowEvents/scripts/ControlFlowEvents.lua
+++ ControlFlowEvents/scripts/ControlFlowEvents.lua
@@ -1,24 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  ControlFlowEvents
-
-  Summary:
-  Introduction of how to notify and react on events from within a ControlFlow
-
-  Description:
-  This sample shows how to notify an event to which a ControlFlow block is
-  registered to. Another event is notified from ControlFlow which a function in
-  this script is registered to.
-  1. Script notifying event -> Controlflow reacts
-  2. Delay of 2000 ms within Controlflow
-  3. ControlFlow notifying event -> Script reacts
-
-  How to run:
-  This sample can be run on any device or on the emulator. After startup the delayed
-  event triggers a function from the script which prints to the console.
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 -- Serving an event which is used to trigger the ControlFlow. Must be served to the Engine CROWN.
